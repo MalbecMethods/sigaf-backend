0ffbfd0eb57fc3b5ea13516404536808df165cc6
aquino alejandro <aquinoalejandro666@gmail.com>
2024-09-25 14:27:33 -0300

    feat: sv funcionando

---
+++ server/src/models/user.ts
@@ -0,0 +1,59 @@
+import { Table, Column, Model, DataType, Default } from "sequelize-typescript";
+import { v4 as uuidv4 } from "uuid";
+
+@Table({ tableName: "users" })
+export class User extends Model {
+    @Default(uuidv4)
+    @Column({
+        type: DataType.UUID,
+        primaryKey: true,
+        allowNull: false,
+    })
+    id: string;
+    
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+        unique: true
+    })
+    username: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    password: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true
+    })
+    nombre: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true
+    })
+    apellido: string;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: true
+    })
+    cuil: number;
+
+    @Column({
+        type: DataType.ENUM('admin', 'user'),
+        allowNull: false,
+        defaultValue: 'user'
+    })
+    role: string;
+
+}
--- server/util/datasets/serializers.py
+++
@@ -1,8 +0,0 @@
-
-from .models import Dataset
-from rest_framework import serializers
-
-class DatasetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dataset
-        fields = '__all__'  
